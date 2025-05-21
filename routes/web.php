b7ee498748d2262ca754b650cbab77c951a76f69
Febby Juwita Hizriani <febbyjuitah@gmail.com>
2025-05-21 16:31:12 +0700

    perubahan web app

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,4 @@ use App\Http\Controllers\DashboardController;
 // });
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
