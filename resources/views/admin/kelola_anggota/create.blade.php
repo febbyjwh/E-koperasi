1769dac10c167700d52e7b34b64e36df478c5948
Febby Juwita Hizriani <febbyjuitah@gmail.com>
2025-09-03 22:01:25 +0700

    testing

--- resources/views/admin/kelola_anggota/create.blade.php
+++ resources/views/admin/kelola_anggota/create.blade.php
@@ -1,14 +1,12 @@
 @extends('layout')
 @section('title', 'Kelola Anggota')
 @section('content')
-
-
     <div class="p-6 bg-white rounded-2xl shadow-lg overflow-x-auto">
         <div class="flex items-center justify-between mb-4">
             <h2 class="text-xl font-semibold text-gray-700">Tambah Anggota</h2>
         </div>
 
-        <form action="{{ route('kelola_anggota.store')}}" method="POST" class="space-y-4">
+        <form action="{{ route('kelola_anggota.store') }}" method="POST" class="space-y-4">
             @csrf
             <div class="mb-5">
                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
@@ -36,7 +34,9 @@
             </div>
             <div class="mb-4">
                 <label for="alamat" class="block text-gray-700 font-medium mb-2">Alamat</label>
-                <textarea name="alamat" id="alamat" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-teal-500 focus:border-teal-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500" required></textarea>
+                <textarea name="alamat" id="alamat"
+                    class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-teal-500 focus:border-teal-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-teal-500 dark:focus:border-teal-500"
+                    required></textarea>
             </div>
             {{-- <div class="mb-4">
                 <label for="jenis_kelamin" class="block text-gray-700 font-medium mb-2">Jenis Kelamin</label>
@@ -55,7 +55,8 @@
             </div> --}}
 
             <div class="flex justify-end gap-2">
-                <button type="submit" class="px-4 py-2 bg-teal-600 text-white rounded-full hover:bg-teal-700 transition cursor-pointer">Simpan</button>
+                <button type="submit"
+                    class="px-4 py-2 bg-teal-600 text-white rounded-full hover:bg-teal-700 transition cursor-pointer">Simpan</button>
                 <a href="{{ route('kelola_anggota.kelola_anggota') }}"
                     class="px-4 py-2 text-gray-600 hover:underline cursor-pointer">
                     Kembali
